f56edb3ccbf3cad4c498903e7917cf32411dfa19
litakelykill <80527108+litakelykill@users.noreply.github.com>
2023-11-19 22:49:45 +0300

    CRUD
    Besoin Achat

--- application/controllers/CT_BesoinAchat.php
+++ application/controllers/CT_BesoinAchat.php
@@ -9,8 +9,29 @@
             parent::__construct();
             $this->load->model('MD_Article');
             $this->load->model('MD_Employe');
+            $this->load->model('MD_Departement');
             $this->load->model('MD_Utilisateur');
             
+        }
+
+        private function viewer($page, $data) {
+            
+            $v = array(
+                'page' => $page,
+                'data' => $data
+            );
+            $this->load->view('template/basepage', $v);
+
+        }
+
+        // Formulaire de demande de besoin
+        public function create() {
+
+            $user = $_SESSION['user'];
+            $data['article'] = $this->MD_Article->listAll();
+            $data['departement'] = $this->MD_Departement->list_Departements();
+            redirect('CT_BesoinAchat/formulaire_demande_besoinachat', $data);
+
         }
 
     }
--- application/models/MD_Departement.php
+++ application/models/MD_Departement.php
@@ -2,18 +2,14 @@
     defined('BASEPATH') OR exit('No direct script access allowed');
 
     class MD_Departement extends CI_Model {
+
         //SELECT
         public function list_Departements() {
-
             $this->db->select("*");
-            $this->db->from('besoin_achat ba');
-            $this->db->join('besoin_achat_final baf', 'ba.idbesoin_achat = baf.idbesoin_achat');
-            $this->db->where('ba.id_article', $id);
-            $this->db->where('ba.etat', $val);
-            $this->db->where('baf.date_finale', 'CURRENT_DATE',  false); 
+            $this->db->from('departement');
             $query = $this->db->get();
             return $query->result();
-        }  
+        }
 
     }
 ?>
