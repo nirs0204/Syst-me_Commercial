21c01f0ef9928caeca2c028eda35849dd37ada85
ChristelRav <ravmihary@gmail.com>
2023-11-21 20:46:16 +0300

    menu

--- application/views/template/basepage.php
+++ application/views/template/basepage.php
@@ -5,17 +5,17 @@ if(!isset($isShopDirector)){ $isShopDirector = array();}
     $this->load->view('template/header');
 
     if($isAllDirector == 1){
-        $this->load->view('template/menu_bar_director');
         $this->load->view('template/menu_head_director');
+        $this->load->view('template/menu_bar_director');
     }
     if($isShopDirector ==1 ){
-        $this->load->view('template/menu_bar_shop');
         $this->load->view('template/menu_head_shop');
+        $this->load->view('template/menu_bar_shop');
     }
 
     if($isAllDirector == 0 && $isShopDirector ==0){
-        $this->load->view('template/menu_bar');
         $this->load->view('template/menu_head');
+        $this->load->view('template/menu_bar');
     }
 
     $this->load->view('pages/'.$page,$data);
