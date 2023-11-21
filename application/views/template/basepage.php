a698f0052c43671925535c7ec28b68b697bee942
ChristelRav <ravmihary@gmail.com>
2023-11-21 08:27:13 +0300

    page_notification

--- application/views/template/basepage.php
+++ application/views/template/basepage.php
@@ -4,17 +4,18 @@ if(!isset($isShopDirector)){ $isShopDirector = array();}
 
     $this->load->view('template/header');
 
-    $this->load->view('template/menu_head');
-
     if($isAllDirector == 1){
         $this->load->view('template/menu_bar_director');
+        $this->load->view('template/menu_head_director');
     }
     if($isShopDirector ==1 ){
         $this->load->view('template/menu_bar_shop');
+        $this->load->view('template/menu_head_shop');
     }
 
     if($isAllDirector == 0 && $isShopDirector ==0){
         $this->load->view('template/menu_bar');
+        $this->load->view('template/menu_head');
     }
 
     $this->load->view('pages/'.$page,$data);
