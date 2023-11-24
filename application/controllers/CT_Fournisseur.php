ff1b8b3e212676d02db7c42a619bfce75c5f6eb0
nirs0204 <118588963+nirs0204@users.noreply.github.com>
2023-11-24 21:07:06 +0300

    formulaire pour les fournisseurs

---
+++ application/controllers/CT_Fournisseur.php
@@ -0,0 +1,50 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class CT_Fournisseur extends CI_Controller {
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('MD_Demande_proforma');
+        $this->load->model('MD_Proforma');
+        $this->load->model('MD_Besoin_achat');
+        $this->load->model('MD_BesoinAchatFinal');
+        $this->load->model('MD_Employe');
+        $this->load->model('MD_Fournisseur');
+        $this->load->model('MD_Utilisateur');
+        $this->load->model('MD_Article');
+        $this->load->library('session');
+
+    }
+    private function viewer($page, $data){
+        if(isset($_SESSION['user'])){
+            $userId = $_SESSION['user']['id_employe'];
+            $tab = $this->MD_Employe->get_admin(  $_SESSION['user']['id_employe']);
+            $dept = $this->MD_Utilisateur->getAll_ByUser($_SESSION['user']['id_utilisateur']);
+            $v = array(
+                'page' => $page,
+                'data' => $data
+            );
+            $v['finance'] = $dept->id_poste;
+            $v['notify'] =  $this->MD_BesoinAchatFinal->notify_Shop(3);
+            $v['notifyr'] =  $this->MD_BesoinAchatFinal->notify_Resp(1,$dept->id_departement);
+            $v['isAllDirector']=$tab[0];
+            $v['isShopDirector']=$tab[1];
+            $this->load->view('template/basepage', $v);
+
+        }else{
+            $v = array(
+                'page' => $page,
+                'data' => $data
+            );
+            $this->load->view('template/basepage', $v);
+        }  
+	}
+    
+    public function index(){
+        // $this->load->view('fournisseur_form');
+        $this->viewer('fournisseur_form');
+    }
+    
+
+}
