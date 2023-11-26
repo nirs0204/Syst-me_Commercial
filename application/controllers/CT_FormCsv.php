c517bed5b54ccee777c8428f8f8f1683849f0e17
litakelykill <80527108+litakelykill@users.noreply.github.com>
2023-11-26 07:47:24 +0300

    Update CT_FormCsv.php

--- application/controllers/CT_FormCsv.php
+++ application/controllers/CT_FormCsv.php
@@ -1,49 +1,51 @@
 <?php
-    defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') OR exit('No direct script access allowed');
 
-    class CT_FormCsv extends CI_Controller {
+use PhpOffice\PhpSpreadsheet\IOFactory;
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
 
-        public function __construct() {        
-            parent::__construct();
-            $this->load->library('Excel');
-        }
+class CT_FormCsv extends CI_Controller {
 
-        public function index() {
-            $this->load->view('pages/creationProforma');
-        }
-
-        public function save() {
-            //$this->request->getPost();
-
-            // Créer un nouvel objet PHPExcel
-            $objPHPExcel = new PHPExcel();
-
-            // Créer un nouvel objet worksheet
-            $objWorksheet = $objPHPExcel->createSheet();
-
-            // Ecrire les données du formulaire dans le  fichier excel
-            $objWorksheet = setCellValue('C2', $this->request->getPost('nom_societe'));
-            $objWorksheet = setCellValue('C3', $this->request->getPost('adresse_societe'));
-            $objWorksheet = setCellValue('C4', $this->request->getPost('telephone'));
-            $objWorksheet = setCellValue('C5', $this->request->getPost('mail'));
-            $objWorksheet = setCellValue('F3', $this->request->getPost('ref_proforma'));
-            $objWorksheet = setCellValue('F4', $this->request->getPost('date_proforma'));
-            $objWorksheet = setCellValue('C8', $this->request->getPost('nom_societe_demandeur'));
-            $objWorksheet = setCellValue('C9', $this->request->getPost('adresse_sd'));
-            $objWorksheet = setCellValue('C10', $this->request->getPost('telephone_sd'));
-            $objWorksheet = setCellValue('C11', $this->request->getPost('email_sd'));
-            $objWorksheet = setCellValue('B7', $this->request->getPost('description'));
-            $objWorksheet = setCellValue('C7', $this->request->getPost('quantite'));
-            $objWorksheet = setCellValue('D7', $this->request->getPost('prix_unitaire'));
-
-            // Sauvegarder le fichier excel
-            $objWriter = PHPExcel_IOFactory::createWriter($objWorksheet, 'Excel');
-            $objWriter->save('D:\Etudes\Projet_Perso\SiK\S5\Mr-Tovo\UwAmp\www\Syst-me_Commercial\uploads\proforma.xslsx');
-
-            // Rediriger sur la page de confirmation
-            return redirect()->to('pages/confirmation');
-        }
-
+    public function __construct() {        
+        parent::__construct();
+        $this->load->library('phpspreadsheet');
     }
 
+    public function index() {
+        $this->load->view('pages/creationProforma');
+    }
+
+    public function save() {
+        $this->load->helper('url');
+
+        // Create a new PhpSpreadsheet object
+        $spreadsheet = new \PhpOffice\PhpSpreadsheet\Spreadsheet();
+
+        // Create a new worksheet
+        $sheet = $spreadsheet->getActiveSheet();
+
+        // Write form data to the Excel file
+        $sheet->setCellValue('C2', $this->input->post('nom_societe'));
+        $sheet->setCellValue('C3', $this->input->post('adresse_societe'));
+        $sheet = setCellValue('C4', $this->request->post('telephone'));
+        $sheet = setCellValue('C5', $this->request->post('mail'));
+        $sheet = setCellValue('F3', $this->request->post('ref_proforma'));
+        $sheet = setCellValue('F4', $this->request->post('date_proforma'));
+        $sheet = setCellValue('C8', $this->request->post('nom_societe_demandeur'));
+        $sheet = setCellValue('C9', $this->request->post('adresse_sd'));
+        $sheet = setCellValue('C10', $this->request->post('telephone_sd'));
+        $sheet = setCellValue('C11', $this->request->post('email_sd'));
+        $sheet = setCellValue('B7', $this->request->post('description'));
+        $sheet = setCellValue('C7', $this->request->post('quantite'));
+        $sheet = setCellValue('D7', $this->request->post('prix_unitaire'));
+
+        // Save the Excel file
+        $writer = IOFactory::createWriter($spreadsheet, 'Xlsx');
+        $writer->save('D:\Etudes\Projet_Perso\SiK\S5\Mr-Tovo\UwAmp\www\Syst-me_Commercial\uploads\proforma.xlsx');
+
+        // Redirect to the confirmation page
+        redirect('pages/confirmation');
+    }
+
+}
 ?>
